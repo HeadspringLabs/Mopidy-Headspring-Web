317e6bd643639566de0b522dc0a6dc5d1f9d91c8
Matthew Wilson <matthew.wilson@headspring.com>
2015-06-11 11:50:40 -0500

    tabs in app bar

--- mopidy_headspring_web/app/components/main-bar.jsx
+++ mopidy_headspring_web/app/components/main-bar.jsx
@@ -1,14 +1,14 @@
 var React = require('react');
 var mui = require('material-ui');
 var AppBar = mui.AppBar;
+var Test = require('./test.jsx');
 
 var MainBar = React.createClass({
   render: function() {
+  	var titleComponent = (<Test/>);
     return (
-      <AppBar title='Jukebox' iconClassNameRight="fa fa-search fa-2x"/>
+      <AppBar title={titleComponent} iconClassNameRight="fa fa-search fa-2x"/>
     );
   }
-  
 });
-
 module.exports = MainBar;
