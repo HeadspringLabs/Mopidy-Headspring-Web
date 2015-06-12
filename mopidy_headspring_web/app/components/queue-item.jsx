fd96e6ed6c81912ba62ef7c6c028157714e91748
Matthew Wilson <matthew.wilson@headspring.com>
2015-06-12 08:16:34 -0500

    more styling

--- mopidy_headspring_web/app/components/queue-item.jsx
+++ mopidy_headspring_web/app/components/queue-item.jsx
@@ -3,14 +3,32 @@ var mui = require('material-ui');
 
 var QueueItem = React.createClass({
   render: function() {
+    var styles = this.getStyles();
   	return (
       <div>
-      	<i class="fa fa-music"></i>
-        <h2>Song Title Here</h2>
-        <h4>Artist name here</h4>
-        <h4>Album name here</h4>
+        <div style={styles.col1}>
+        	<i className="fa fa-music fa-3x"></i>
+        </div>
+        <div style={styles.col2}>
+          <h2> Song Title Here</h2>
+          <h4>Artist name here</h4>
+          <h4>Album name here</h4>
+        </div>
       </div>
     );
+  },
+  getStyles: function() {
+    var styles = {
+      col1: {
+        display: 'inline-block',
+        width: '25%',
+      },
+      col2: {
+        display: 'inline-block',
+        width: '75%',
+      },
+    };
+    return styles;
   },
 });
 module.exports = QueueItem;
